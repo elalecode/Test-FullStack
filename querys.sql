ff48edd2204d231f1b0405ea2f38e614835a2bfa
Alexandro Jimenez <31660447+elalecode@users.noreply.github.com>
2022-12-28 19:06:27 -0600

    Consultas del ejercicio 1

--- querys.sql
+++ querys.sql
@@ -1,1 +1,12 @@
 -- Escribe tus querys acá
+
+-- Clientes que pagaron con transbank
+select c.email
+from venta as v
+inner join cliente as c on c.id = v.cliente_id
+where v.pagado_por == "transbank";
+
+
+select i.id
+from cliente, venta, informe
+where cliente.email = "juan_daniel@gmail.com" and cliente.id = venta.cliente_id and informe.id = venta.informe_id;
